75e02115ec74cdf34f1c05b522425c101afa395f
quockhanh1702 <74188095+quockhanh1702@users.noreply.github.com>
2021-07-05 22:43:17 +0700

    User(con thieu cart se bo sung them)

---
+++ eProject/OnlineArtGallery/OnlineArtGallery/Controllers/IndexController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineArtGallery.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+        public IActionResult MyBid()
+        {
+            return View();
+        }
+        public IActionResult WinningBid()
+        {
+            return View();
+        }
+        public IActionResult MyAlerts()
+        {
+            return View();
+        }
+        public IActionResult MyFavorite()
+        {
+            return View();
+        }
+        public IActionResult Referral()
+        {
+            return View();
+        }
+    }
+}
