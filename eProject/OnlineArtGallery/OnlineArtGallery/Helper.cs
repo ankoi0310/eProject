53bbf6b83e1dc70bccc3a02bbd577d09b80c20f9
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-11 12:09:06 +0700

    add helper.cs

---
+++ eProject/OnlineArtGallery/OnlineArtGallery/Helper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using OnlineArtGallery.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineArtGallery
+{
+    public class Helper
+    {
+        public static string RenderRazorViewString(Controller controller, string viewName, object model = null)
+        {
+            controller.ViewData.Model = model;
+            StringWriter sw = new();
+            IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+            ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, false);
+
+            ViewContext viewContext = new(
+                controller.ControllerContext,
+                viewResult.View,
+                controller.ViewData,
+                controller.TempData,
+                sw,
+                new HtmlHelperOptions()
+            );
+            viewResult.View.RenderAsync(viewContext);
+            return sw.GetStringBuilder().ToString();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class NoDirectAccessAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.GetTypedHeaders().Referer == null || context.HttpContext.Request.GetTypedHeaders().Host.Host.ToString() != context.HttpContext.Request.GetTypedHeaders().Referer.Host.ToString())
+            {
+                context.HttpContext.Response.Redirect("/");
+            }
+        }
+    }
+}
