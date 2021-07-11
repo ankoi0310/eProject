c2a36fea43e088e524a4de2fdbed9830685a3900
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-11 12:14:10 +0700

    create Tools

---
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/Tools.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineArtGallery.Models
+{
+    public static partial class Tools
+    {
+        private static readonly DBContext context = new();
+
+        // User Type
+        public static IEnumerable<UserType> GetUserTypeList() => context.UserTypes.ToList();
+        public static string GetUsetType(int id) => context.UserTypes.Find(id).Name;
+
+        // User
+        public static User GetUser(int id) => context.Users.Find(id);
+        
+    }
+}
