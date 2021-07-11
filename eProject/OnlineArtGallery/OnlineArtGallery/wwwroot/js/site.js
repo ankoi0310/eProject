5f62f356e161ed6616864edc081638627619c328
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-11 12:03:50 +0700

    update site.js

--- eProject/OnlineArtGallery/OnlineArtGallery/wwwroot/js/site.js
+++ eProject/OnlineArtGallery/OnlineArtGallery/wwwroot/js/site.js
@@ -2,3 +2,96 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+showInPopup = (url, title) => {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (res) {
+            $("#form-modal .modal-body").html(res);
+            $("#form-modal .modal-title").html(title);
+            $("#form-modal").modal("show");
+        }
+    })
+}
+
+jQueryAjaxPost = (form) => {
+    //$('#editor').html() == "<p><br data-cke-filler=\"true\"></p>" ? $('#Content').val(null) : $('#Content').val($('#editor').html());
+    try {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    $("#view-all").html(res.html);
+                    setInterval('location.reload()', 100);
+                    $("#form-modal").modal('hide');
+                    $("#form-modal .modal-body").html('');
+                    $("#form-modal .modal-title").html('');
+                } else {
+                    $("#form-modal .modal-body").html(res.html);
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+
+    return false;
+}
+
+jQueryAjaxPostForRegister = (form) => {
+    try {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    alert("Submitted Succesfully");
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+
+    return false;
+}
+
+jQueryAjaxDelete = form => {
+    if (confirm("Are you sure to delete this record?")) {
+        try {
+            $.ajax({
+                type: "POST",
+                url: form.action,
+                data: new FormData(form),
+                contentType: false,
+                processData: false,
+                success: function (res) {
+                    $("#view-all").html(res.html);
+                    setInterval('location.reload()', 100);
+                    $.notify("Deleted successfully", { globalPosition: "top center", className: "success" });
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return false;
+}
