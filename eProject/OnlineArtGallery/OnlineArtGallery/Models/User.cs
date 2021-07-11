16d965e7f6d2ba811d483feec8a238a44515a86f
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-11 12:06:34 +0700

    update user

--- eProject/OnlineArtGallery/OnlineArtGallery/Models/User.cs
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/User.cs
@@ -10,21 +10,21 @@ namespace OnlineArtGallery.Models
     [Table("User")]
     public class User
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("Id", TypeName = "int")]
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Column("Id", TypeName = "int")]
         public int Id { get; set; }
-        [Column("Username", TypeName = "nvarchar")]
-        [StringLength(50)]
+        //[Column("Username", TypeName = "nvarchar")]
+        //[StringLength(50)]
         public string Username { get; set; }
-        [Column("Password", TypeName = "nvarchar")]
-        [StringLength(50)]
+        //[Column("Password", TypeName = "nvarchar")]
+        //[StringLength(50)]
         public string Password { get; set; }
-        [Column("UsertypeId", TypeName = "int")]
+        //[Column("UsertypeId", TypeName = "int")]
         public int UsertypeId { get; set; }
-        [ForeignKey("UsertypeId")]
-        public UserType UserType { get; set; }
-        [Column("Active", TypeName = "bit")]
+        //[ForeignKey("UsertypeId")]
+        //public UserType UserType { get; set; }
+        //[Column("Active", TypeName = "bit")]
         public bool Active { get; set; }
     }
 }
