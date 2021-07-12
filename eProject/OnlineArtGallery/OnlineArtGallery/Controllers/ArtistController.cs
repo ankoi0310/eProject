77abbb0bd84291a731599ccd0d4dcceaeeaf6265
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-12 20:19:55 +0700

    update ArtistController

--- eProject/OnlineArtGallery/OnlineArtGallery/Controllers/ArtistController.cs
+++ eProject/OnlineArtGallery/OnlineArtGallery/Controllers/ArtistController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineArtGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,96 @@ namespace OnlineArtGallery.Controllers
 {
     public class ArtistController : Controller
     {
+        private readonly DBContext _context;
+
+        public ArtistController()
+        {
+            _context ??= new DBContext();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        // GET: Artist
+        public async Task<IActionResult> List()
+        {
+            return View(await _context.Artists.ToListAsync());
+        }
+
+        // GET: Artist/AddOrEdit
+        // GET: Artist/AddOrEdit/5
+        [NoDirectAccess]
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return base.View(new Artist());
+            }
+            else
+            {
+                var artist = await _context.Artists.FindAsync(id);
+                if (artist == null)
+                {
+                    return NotFound();
+                }
+                return View(artist);
+            }
+        }
+
+        // POST: Artist/AddOrEdit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("Id,Name,Gender,Birthday,Phone,Email,Address,Image,Major,Achivement,UserId,Active")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == 0)
+                {
+                    _context.Add(artist);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    try
+                    {
+                        _context.Update(artist);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!ArtistExists(artist.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+                return Json(new { isValid = true, html = Helper.RenderRazorViewString(this, "_ViewAll", _context.Artists.ToList()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewString(this, "AddOrEdit", artist) });
+        }
+
+        // POST: Artist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artist = await _context.Artists.FindAsync(id);
+            _context.Artists.Remove(artist);
+            await _context.SaveChangesAsync();
+            return Json(new { html = Helper.RenderRazorViewString(this, "_ViewAll", _context.Artists.ToList()) });
+        }
+
+        private bool ArtistExists(int id)
+        {
+            return _context.Artists.Any(e => e.Id == id);
         }
     }
 }
