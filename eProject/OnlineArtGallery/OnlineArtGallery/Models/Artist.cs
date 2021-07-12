dd10d003b5079c04b23e90adde2622aac875f545
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-12 20:20:52 +0700

    create artist models

--- eProject/OnlineArtGallery/OnlineArtGallery/Models/Artist.cs
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/Artist.cs
@@ -10,43 +10,17 @@ namespace OnlineArtGallery.Models
     [Table("Artist")]
     public class Artist
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("Id", TypeName = "int")]
         public int Id { get; set; }
-        [Column("Name", TypeName = "nvarchar")]
-        [StringLength(50)]
         public string Name { get; set; }
-        [Column("Gender", TypeName = "bit")]
         public bool Gender { get; set; }
-        [Column("Birthday", TypeName = "date")]
         public DateTime Birthday { get; set; }
-        [Column("Phone", TypeName = "nvarchar")]
-        [StringLength(50)]
         public string Phone { get; set; }
-        [Column("Email", TypeName = "nvarchar")]
-        [StringLength(50)]
         public string Email { get; set; }
-        [Column("Address", TypeName = "nvarchar")]
         public string Address { get; set; }
-        [Column("Image", TypeName = "nvarchar")]
         public string Image { get; set; }
-        [Column("Major", TypeName = "nvarchar")]
-        [StringLength(50)]
         public string Major { get; set; }
-        [Column("Achivement", TypeName = "nvarchar")]
         public string Achivement { get; set; }
-        [Column("BankAccount", TypeName = "nvarchar")]
-        [StringLength(50)]
-        public string BankAccount { get; set; }
-        [Column("BankName", TypeName = "nvarchar")]
-        [StringLength(50)]
-        public string BankName { get; set; }
-        [Column("UserId", TypeName = "int")]
         public int UserId { get; set; }
-        [ForeignKey("UserId")]
-        public User User { get; set; }
-        [Column("Active", TypeName = "bit")]
         public bool Active { get; set; }
 
     }
