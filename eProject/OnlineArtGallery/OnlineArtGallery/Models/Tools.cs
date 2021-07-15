cb9d6123115b72adb0f0ba7e69dc7e3d8d05bb9e
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-16 00:21:17 +0700

    add encrypt/decrypt function

--- eProject/OnlineArtGallery/OnlineArtGallery/Models/Tools.cs
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/Tools.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Text;
+using System.Security.Cryptography;
+using System.Security;
 
 namespace OnlineArtGallery.Models
 {
@@ -21,9 +24,11 @@ namespace OnlineArtGallery.Models
         public static User GetUser(int id) => context.Users.Find(id);
 
         // Customer
+        public static IEnumerable<Customer> GetCustomerList() => context.Customers.ToList();
         public static Customer GetCustomerFromUser(int userId) => (from a in context.Customers where a.UserId == userId select a).FirstOrDefault();
 
         // Artist
+        public static IEnumerable<Artist> GetArtistList() => context.Artists.ToList();
         public static Artist GetArtistFromUser(int userId) => (from a in context.Artists where a.UserId == userId select a).FirstOrDefault();
 
         // Art Category
@@ -32,7 +37,7 @@ namespace OnlineArtGallery.Models
         //get User from Session
         public static User GetUserfromSession(string userString)
         {
-            User user = new User();
+            User user = new();
             if (!String.IsNullOrEmpty(userString))
             {
                 user = JsonConvert.DeserializeObject<User>(userString);    
@@ -44,6 +49,56 @@ namespace OnlineArtGallery.Models
         public static bool CheckAuctionExist(int id)
         {
             return context.Auctions.Any(x => x.ArtworkId == id);
+        }
+
+        /// <summary>
+        /// Encrypts a given password and returns the encrypted data
+        /// as a base64 string.
+        /// </summary>
+        /// <param name="plainText">An unencrypted string that needs
+        /// to be secured.</param>
+        /// <returns>A base64 encoded string that represents the encrypted
+        /// binary data.
+        /// </returns>
+        /// <remarks>This solution is not really secure as we are
+        /// keeping strings in memory. If runtime protection is essential,
+        /// <see cref="SecureString"/> should be used.</remarks>
+        /// <exception cref="ArgumentNullException">If <paramref name="plainText"/>
+        /// is a null reference.</exception>
+        public static string Encrypt(string plainText)
+        {
+            if (plainText == null) throw new ArgumentNullException(nameof(plainText));
+
+            //encrypt data
+            var data = Encoding.Unicode.GetBytes(plainText);
+            byte[] encrypted = ProtectedData.Protect(data, null, DataProtectionScope.LocalMachine);
+
+            //return as base64 string
+            return Convert.ToBase64String(encrypted);
+        }
+
+        /// <summary>
+        /// Decrypts a given string.
+        /// </summary>
+        /// <param name="cipher">A base64 encoded string that was created
+        /// through the <see cref="Encrypt(string)"/> or
+        /// <see cref="Encrypt(SecureString)"/> extension methods.</param>
+        /// <returns>The decrypted string.</returns>
+        /// <remarks>Keep in mind that the decrypted string remains in memory
+        /// and makes your application vulnerable per se. If runtime protection
+        /// is essential, <see cref="SecureString"/> should be used.</remarks>
+        /// <exception cref="ArgumentNullException">If <paramref name="cipher"/>
+        /// is a null reference.</exception>
+        public static string Decrypt(string cipher)
+        {
+            if (cipher == null) throw new ArgumentNullException(nameof(cipher));
+
+            //parse base64 string
+            byte[] data = Convert.FromBase64String(cipher);
+
+            //decrypt data
+            byte[] decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
+            return Encoding.Unicode.GetString(decrypted);
         }
     }
 }
