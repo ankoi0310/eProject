041c0d943d4e63553a9230ab545be9f8d217042b
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-20 22:05:41 +0700

    create update object

---
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/UserUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineArtGallery.Models
+{
+    public class UserUpdate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Image { get; set; }
+        public int UserId { get; set; }
+    }
+}
