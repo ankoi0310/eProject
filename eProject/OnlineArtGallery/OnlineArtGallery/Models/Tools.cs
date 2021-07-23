6f57e82fa91a17a6800753c91731c84df3eecef5
huynhvanhuuan <19130003@st.hcmuaf.edu.vn>
2021-07-23 15:55:30 +0700

    repair encrypt decrypt

--- eProject/OnlineArtGallery/OnlineArtGallery/Models/Tools.cs
+++ eProject/OnlineArtGallery/OnlineArtGallery/Models/Tools.cs
@@ -114,14 +114,14 @@ namespace OnlineArtGallery.Models
         /// is a null reference.</exception>
         public static string Encrypt(string plainText)
         {
-            if (plainText == null) throw new ArgumentNullException(nameof(plainText));
+            //if (plainText == null) throw new ArgumentNullException(nameof(plainText));
 
             //encrypt data
             var data = Encoding.Unicode.GetBytes(plainText);
-            byte[] encrypted = ProtectedData.Protect(data, null, DataProtectionScope.LocalMachine);
+            //byte[] encrypted = ProtectedData.Protect(data, null, DataProtectionScope.LocalMachine);
 
             //return as base64 string
-            return Convert.ToBase64String(encrypted);
+            return Convert.ToBase64String(data);
         }
 
         /// <summary>
@@ -138,14 +138,14 @@ namespace OnlineArtGallery.Models
         /// is a null reference.</exception>
         public static string Decrypt(string cipher)
         {
-            if (cipher == null) throw new ArgumentNullException(nameof(cipher));
+            //if (cipher == null) throw new ArgumentNullException(nameof(cipher));
 
             //parse base64 string
             byte[] data = Convert.FromBase64String(cipher);
 
             //decrypt data
-            byte[] decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
-            return Encoding.Unicode.GetString(decrypted);
+            //byte[] decrypted = ProtectedData.Unprotect(data, null, DataProtectionScope.CurrentUser);
+            return Encoding.Unicode.GetString(data);
         }
     }
 }
